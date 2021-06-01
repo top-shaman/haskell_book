aa1dae28d8b6bff1a669e995b9cc9f8cd988edf7
top-shaman <jaiq@Jaiqs-MacBook-Pro.local>
2021-06-01 16:33:47 -0700

    99problems 1-10

--- ch9/myLength.hs
+++ ch9/myLength.hs
@@ -2,4 +2,6 @@ module Mylength where
 
 myLength :: [a] -> Integer
 myLength [] = 0
-myLength (_:xs) = 1 + myLength xs
+myLength (_:xs) = 1 + myLength xs -- we use '_' to ignore values in our arguments that are part of a pattern match
+-- strict in the spine, but not the values
+-- forces evaluation of the entire spine without accompanying strictness in values
